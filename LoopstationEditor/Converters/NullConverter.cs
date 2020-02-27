46de79f3068e348df37943a1ea9d2d65b57450d6
wRadion <wradion@gmail.com>
2020-02-27 15:14:08 +0100

    Fixed issue with ImageSource <null>

---
+++ LoopstationEditor/Converters/NullConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LoopstationEditor.Converters
+{
+    public class NullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? DependencyProperty.UnsetValue : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}

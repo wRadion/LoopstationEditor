e1dec49d4ac333cdac332f7b28f5ad55cc7cf918
wRadion <wradion@gmail.com>
2020-02-29 16:33:41 +0100

    Whole Loopstation Views (interface only) DONE! (AND CLEAN!)

---
+++ LoopstationEditor/Views/Loopstation/Tracks/Controls/Track.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LoopstationEditor.Views.Loopstation.Tracks.Controls
+{
+    /// <summary>
+    /// Interaction logic for Track.xaml
+    /// </summary>
+    public partial class Track : UserControl
+    {
+        public static DependencyProperty TrackNumberProperty = DependencyProperty.Register("TrackNumber", typeof(int), typeof(Track));
+
+        public int TrackNumber
+        {
+            get => (int)GetValue(TrackNumberProperty);
+            set => SetValue(TrackNumberProperty, value);
+        }
+
+        public Track()
+        {
+            InitializeComponent();
+        }
+    }
+}

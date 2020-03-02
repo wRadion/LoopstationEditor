470ba0c8dad09d8f6e6b721dee44ba82de2c4528
wRadion <wradion@gmail.com>
2020-03-02 10:50:11 +0100

    (Xml): Throws a custom ValueOutOfRangeException instead of an ArgumentOutOfRange in Range.Guard

---
+++ LoopstationXml/Exceptions/ValueOutOfRangeException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Loopstation.Xml.Exceptions
+{
+    public class ValueOutOfRangeException : Exception
+    {
+        public ValueOutOfRangeException(int value, int min, int max)
+            : base($"Integer value `{ value }' should be between `{ min }' and `{ max }'.") { }
+    }
+}
--- LoopstationXml/Utils/Range.cs
+++ LoopstationXml/Utils/Range.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Loopstation.Xml.Exceptions;
 
 namespace Loopstation.Xml.Utils
 {
@@ -6,7 +6,7 @@ namespace Loopstation.Xml.Utils
     {
         /// <summary>
         /// Ensure that the given value is between the minimum and maximum value provided.
-        /// <para>Throws a <c>ArgumentOutOfRangeException</c> if the value is not in the given range.</para>
+        /// <para>Throws a <c>ValueOutOfRangeException</c> if the value is not in the given range.</para>
         /// <para>Returns the integer value as is if the value is in the given range.</para>
         /// </summary>
         /// <param name="value">The value to test</param>
@@ -16,7 +16,7 @@ namespace Loopstation.Xml.Utils
         public static int Guard(int value, int min = 0, int max = 100)
         {
             if (min > value || value > max)
-                throw new ArgumentOutOfRangeException($"Integer value `{ value }' should be between `{ min }' and `{ max }'.");
+                throw new ValueOutOfRangeException(value, min, max);
 
             return value;
         }

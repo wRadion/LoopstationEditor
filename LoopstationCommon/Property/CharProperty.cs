2032970ef9b71b92b8eeb89598724c0f7641b6b6
wRadion <wradion@gmail.com>
2020-03-02 19:09:18 +0100

    [Common]: Added CharProperty

---
+++ LoopstationCommon/Property/CharProperty.cs
@@ -0,0 +1,15 @@
+﻿namespace Loopstation.Common.Property
+{
+    public class CharProperty : PropertyBase<char>
+    {
+        public CharProperty(int value) : base(value) { }
+        public CharProperty(char value) : base(value) { }
+
+        public override int Convert() => Value;
+
+        public static implicit operator int(CharProperty prop) => prop.Value;
+        public static implicit operator char(CharProperty prop) => prop.Value;
+        public static implicit operator CharProperty(int value) => new CharProperty(value);
+        public static implicit operator CharProperty(char value) => new CharProperty(value);
+    }
+}

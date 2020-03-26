95044ddc5ab9e69cf922ef6eb7abd2a6fb4746a5
wRadion <wradion@gmail.com>
2020-03-26 18:05:03 +0100

    Bump Beta v0.1

--- LoopstationEditor/Properties/AssemblyInfo.cs
+++ LoopstationEditor/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LoopstationEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Loopstation Settings Editor")]
+[assembly: AssemblyDescription("A simple tool for editing RC-505 Loopstation settings.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("wRadion")]
 [assembly: AssemblyProduct("LoopstationEditor")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
